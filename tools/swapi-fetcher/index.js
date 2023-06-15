18c486700122c526cf3dad7a1f3246b9c4543af9
Jamison Bryant <jamison@akooba.com>
2023-06-15 00:05:40 -0400

    Fetcher script is generating SQL now

--- tools/swapi-fetcher/index.js
+++ tools/swapi-fetcher/index.js
@@ -2,71 +2,106 @@ const axios = require('axios');
 
 // Fetch data from SWAPI and process it
 async function fetchData(url) {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error).message;
+        // throw error;
+    }
 }
 
 // Generate SQL INSERT statements for people table
 function generatePeopleSql(people) {
-  return people.map(person => {
-    return `INSERT INTO people (id, name, birth_year, eye_color, hair_color, height, mass, planet_id, species_id) VALUES (${person.id}, '${person.name}', '${person.birth_year}', '${person.eye_color}', '${person.hair_color}', '${person.height}', '${person.mass}', ${person.homeworld.match(/\d+/)}, ${person.species.length > 0 ? person.species[0].match(/\d+/) : 'NULL'});`;
-  });
+    return people.map((person) => {
+        const { name, birth_year, eye_color, hair_color, height, mass, homeworld, species } = person;
+        const planetId = homeworld?.match(/\d+/);
+        const speciesId = species.length > 0 ? species[0].match(/\d+/) : 'NULL';
+
+        return `INSERT INTO people (name, birth_year, eye_color, hair_color, height, mass, planet_id, species_id) VALUES ('${name}', '${birth_year}', '${eye_color}', '${hair_color}', '${height}', '${mass}', ${planetId}, ${speciesId});`;
+    });
 }
 
 // Generate SQL INSERT statements for planets table
 function generatePlanetsSql(planets) {
-  return planets.map(planet => {
-    return `INSERT INTO planets (id, name, climate, diameter, orbital_period, gravity, terrain, population) VALUES (${planet.id}, '${planet.name}', '${planet.climate}', '${planet.diameter}', '${planet.orbital_period}', '${planet.gravity}', '${planet.terrain}', '${planet.population}');`;
-  });
+    return planets.map((planet) => {
+        const { name, climate, diameter, orbital_period, gravity, terrain, population } = planet;
+
+        return `INSERT INTO planets (name, climate, diameter, orbital_period, gravity, terrain, population) VALUES ('${name}', '${climate}', '${diameter}', '${orbital_period}', '${gravity}', '${terrain}', '${population}');`;
+    });
 }
 
 // Generate SQL INSERT statements for species table
 function generateSpeciesSql(species) {
-  return species.map(specie => {
-    return `INSERT INTO species (id, name, classification, designation, average_height, average_lifespan, language, planet_id) VALUES (${specie.id}, '${specie.name}', '${specie.classification}', '${specie.designation}', '${specie.average_height}', '${specie.average_lifespan}', '${specie.language}', ${specie.homeworld.match(/\d+/)});`;
-  });
+    return species.map((specie) => {
+        const { name, classification, designation, average_height, average_lifespan, language, homeworld } = specie;
+        const planetId = homeworld?.match(/\d+/);
+
+        return `INSERT INTO species (name, classification, designation, average_height, average_lifespan, language, planet_id) VALUES ('${name}', '${classification}', '${designation}', '${average_height}', '${average_lifespan}', '${language}', ${planetId});`;
+    });
 }
 
 // Generate SQL INSERT statements for vehicles table
 function generateVehiclesSql(vehicles) {
-  return vehicles.map(vehicle => {
-    return `INSERT INTO vehicles (id, name, manufacturer, model, vehicle_class, passengers, crew, cargo_capacity, owner_id) VALUES (${vehicle.id}, '${vehicle.name}', '${vehicle.manufacturer}', '${vehicle.model}', '${vehicle.vehicle_class}', '${vehicle.passengers}', '${vehicle.crew}', '${vehicle.cargo_capacity}', ${vehicle.pilots.length > 0 ? vehicle.pilots[0].match(/\d+/) : 'NULL'});`;
-  });
+    return vehicles.map((vehicle) => {
+        const { name, manufacturer, model, vehicle_class, passengers, crew, cargo_capacity, pilots } = vehicle;
+        const ownerId = pilots.length > 0 ? pilots[0].match(/\d+/) : 'NULL';
+
+        return `INSERT INTO vehicles (name, manufacturer, model, vehicle_class, passengers, crew, cargo_capacity, owner_id) VALUES ('${name}', '${manufacturer}', '${model}', '${vehicle_class}', '${passengers}', '${crew}', '${cargo_capacity}', ${ownerId});`;
+    });
 }
 
 // Main function to fetch SWAPI data and generate SQL INSERT statements
 async function generateSqlInsertStatements() {
-  try {
-    const peopleData = await fetchData('https://swapi.dev/api/people');
-    const planetsData = await fetchData('https://swapi.dev/api/planets');
-    const speciesData = await fetchData('https://swapi.dev/api/species');
-    const vehiclesData = await fetchData('https://swapi.dev/api/vehicles');
+    try {
+        let peopleData = [];
+        const peopleCount = 10;
+        for (let i = 0; i < peopleCount; i++) {
+            data = await fetchData(`https://swapi.dev/api/people/${i}`)
+                .then((data) => data && peopleData.push(data));
+        }
 
-    const peopleInsertStatements = generatePeopleSql(peopleData.results);
-    const planetsInsertStatements = generatePlanetsSql(planetsData.results);
-    const speciesInsertStatements = generateSpeciesSql(speciesData.results);
-    const vehiclesInsertStatements = generateVehiclesSql(vehiclesData.results);
+        let planetsData = [];
+        const planetCount = 10;
+        for (let i = 0; i < planetCount; i++) {
+            data = await fetchData(`https://swapi.dev/api/planets/${i}`)
+                .then((data) => data && planetsData.push(data));
+        }
 
-    // Output the SQL INSERT statements to stdout
-    console.log('-- INSERT statements for people table --');
-    peopleInsertStatements.forEach(statement => console.log(statement));
+        let speciesData = [];
+        const speciesCount = 10;
+        for (let i = 0; i < speciesCount; i++) {
+            data = await fetchData(`https://swapi.dev/api/species/${i}`)
+                .then((data) => data && speciesData.push(data));
+        }
 
-    console.log('\n-- INSERT statements for planets table --');
-    planetsInsertStatements.forEach(statement => console.log(statement));
+        let vehiclesData = [];
+        const vehicleCount = 10;
+        for (let i = 0; i < vehicleCount; i++) {
+            await fetchData(`https://swapi.dev/api/vehicles/${i}`)
+                .then((data) => data && vehiclesData.push(data));
+        }
 
-    console.log('\n-- INSERT statements for species table --');
-    speciesInsertStatements.forEach(statement => console.log(statement));
+        const peopleInsertStatements = generatePeopleSql(peopleData);
+        const planetsInsertStatements = generatePlanetsSql(planetsData);
+        const speciesInsertStatements = generateSpeciesSql(speciesData);
+        const vehiclesInsertStatements = generateVehiclesSql(vehiclesData);
 
-    console.log('\n-- INSERT statements for vehicles table --');
-    vehiclesInsertStatements.forEach(statement => console.log(statement));
-  } catch (error) {
-    console.error('Error generating SQL insert statements:', error);
-  }
+        // Output the SQL INSERT statements to stdout
+        console.log('-- INSERT statements for people table --');
+        peopleInsertStatements.forEach((statement) => console.log(statement));
+
+        console.log('\n-- INSERT statements for planets table --');
+        planetsInsertStatements.forEach((statement) => console.log(statement));
+
+        console.log('\n-- INSERT statements for species table --');
+        speciesInsertStatements.forEach((statement) => console.log(statement));
+
+        console.log('\n-- INSERT statements for vehicles table --');
+        vehiclesInsertStatements.forEach((statement) => console.log(statement));
+    } catch (error) {
+        console.error('Error generating SQL insert statements:', error);
+    }
 }
 
 // Run the script
