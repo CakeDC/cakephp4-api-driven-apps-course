69d9dd3f0be1bd9d1e2f8c1040488a7db0114c64
Jamison Bryant <jamison@akooba.com>
2023-07-14 23:57:45 -0400

    Update layout & add header bar

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-container>
+            <b-navbar-brand href="#">
+                <div class="d-flex align-items-center">
+                    <b-icon-bezier class="h2 mr-2 text-white"></b-icon-bezier>
+                    <span class="h3 text-white">&nbsp;CakePHP API-Vue Demo</span>
+                </div>
+            </b-navbar-brand>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item>
+                    <!-- TODO: repo link -->
+                    <!-- TODO: course link -->
+                    <!-- TODO: cakedc link -->
+                </b-nav-item>
+            </b-navbar-nav>
+        </b-container>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: "HeaderBar",
+};
+</script>
